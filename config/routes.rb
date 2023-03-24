3b46efbbea320cfd3bad7b740b0a5b7623a75683
Mickey254-ja <mickey.awino@student.moringaschool.com>
2023-03-24 19:29:53 +0300

    completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
+  #login
+  post "/login", to: "sessions#create"
+  #stay logged in
+  get "/me", to: "users#show"
+  #logout
+  delete "/logout",to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
